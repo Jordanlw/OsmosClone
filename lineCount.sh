1eee2f4a1109486c42705159c5e5ac355b611ca5
Jordan W <jordanw2@gmail.com>
2011-05-25 11:05:49 +0930

    Prevented linecount.sh from displaying directories.

--- lineCount.sh
+++ lineCount.sh
@@ -1,1 +1,1 @@
-find . | grep -v Compile.sh | grep -v .git | grep -v Resource | grep -v xOsmosClone | grep -v lineCount.sh |xargs cat | wc -l
+find . -type f| grep -v Compile.sh | grep -v .git | grep -v Resource | grep -v xOsmosClone | grep -v lineCount.sh |xargs cat | wc -l
