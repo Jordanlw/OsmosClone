4acb013ac9e713f140983d93752526bff41aaa7a
Jordan W <jordanw2@gmail.com>
2011-07-31 09:43:15 +0930

    movementBoundCheck now works on radius rather than x,y potion.

--- movementBoundCheck.c
+++ movementBoundCheck.c
@@ -1,20 +1,8 @@
 void movementBoundCheck(int i)
 {
 	object *objects = (object *)objectStore(NULL,2);
-	if(objects[i].pos.x > LEVEL_WIDTH)
-	{
-		objects[i].pos.x = LEVEL_WIDTH;
-	}
-	else if(objects[i].pos.x < 0)
-	{
-		objects[i].pos.x = 0;
-	}
-	if(objects[i].pos.y > LEVEL_HEIGHT)
-	{
-		objects[i].pos.y = LEVEL_HEIGHT;
-	}
-	else if(objects[i].pos.y < 0)
-	{
-		objects[i].pos.y = 0;
-	}
+	if(objects[i].pos.x - objects[i].radius < 0) objects[i].pos.x = objects[i].radius;
+	if(objects[i].pos.x + objects[i].radius > LEVEL_WIDTH) objects[i].pos.x = LEVEL_WIDTH - objects[i].radius;
+	if(objects[i].pos.y - objects[i].radius < 0) objects[i].pos.y = objects[i].radius;
+	if(objects[i].pos.y + objects[i].radius > LEVEL_HEIGHT) objects[i].pos.y = LEVEL_HEIGHT - objects[i].radius;
 }
