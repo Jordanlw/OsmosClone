4ee980e91798b8355a0035e84066e80000a66b75
Jordan W <jordanw2@gmail.com>
2011-10-14 21:40:02 +1030

    Simple ai in place, avoids threats when no prey available, targets prey when possible.

--- aimovement.c
+++ aimovement.c
@@ -22,37 +22,35 @@ void moveAiObjects()
 	{
 		if(i == player || obj[i].radius <= 0) continue;
 		//Loop for objects to find target
-		int target = -1, biggestPredator = -1;
+		int target = -1, bigPred = 0, bigPredSet = 0;
 		//Storage for value to determine prefence to targets for predator attacks
-		int targetPrefValue = 0;
-		int relX, relY;
+		int targetPrefValue = -1;
+		vector relPos;
 		int j;
 		for(j = 0;j < OBJECTS;j++)
 		{
-			if(j == i || obj[j].radius <= 0 || obj[i].radius < obj[j].radius) continue;
-			relX = obj[j].pos.x - obj[i].pos.x;
-			relY = obj[j].pos.y - obj[i].pos.y;
-			int prefValue = obj[j].radius * sqrt(relX * relX + relY * relY);
-			if(prefValue < targetPrefValue || targetPrefValue == 0)
+			if(j == i || obj[j].radius <= 0) continue;
+			relPos.x = obj[j].pos.x - obj[i].pos.x;
+			relPos.y = obj[j].pos.y - obj[i].pos.y;
+			int prefValue = obj[j].radius * sqrt(relPos.x * relPos.x + relPos.y * relPos.y);
+			if((prefValue < targetPrefValue || targetPrefValue == -1) && obj[j].radius <= obj[i].radius)
 			{
 				targetPrefValue = prefValue;
 				target = j;
 			}
-			if(prefValue > targetPrefValue) biggestPredator = j;
+			if(obj[j].radius >= obj[bigPred].radius) bigPred = j, bigPredSet = 1;
 		}
 		//If target hasn't been found, skip the rest (movement to target)
-		if(target == -1 && biggestPredator == -1) continue;
-		if(biggestPredator != -1 && target == -1)
+		if(target == -1 && bigPredSet == 0) continue;
+		if(bigPredSet == 1 && targetPrefValue == -1)
 		{
-			relX = obj[i].pos.x - obj[biggestPredator].pos.x;
-			relY = obj[i].pos.y - obj[biggestPredator].pos.y;
+			relPos = (vector){obj[i].pos.x - obj[bigPred].pos.x,obj[i].pos.y - obj[bigPred].pos.y};
 		}
-		vector normedRel = (vector){relX,relY};
-		normalize(&normedRel);
-		obj[i].force.x += normedRel.x * SPEED;
-		obj[i].force.y += normedRel.y * SPEED;
-		//DEBUG
-		//printf("p: %d t: %d\n",i,target);
-		//printf("x: %f y: %f d: %f\n",normedRel.x,normedRel.y,sqrt(normedRel.x * normedRel.x + normedRel.y * normedRel.y));
+		vector heading = obj[i].vel;
+		relPos.x -= heading.x;
+		relPos.y -= heading.y;
+		normalize(&relPos);
+		obj[i].force.x += SPEED * relPos.x;
+		obj[i].force.y += SPEED * relPos.y;
 	}
 }
