897b838c723f86f91b8210823feeb394851f7905
Jordan W <jordanw2@gmail.com>
2011-10-14 21:32:57 +1030

    Prevented division by zero in vectorMath.c:normalize()

--- vectorMath.c
+++ vectorMath.c
@@ -5,6 +5,10 @@
 void normalize(vector *A)
 {
 	double length = sqrt(A->x * A->x + A->y * A->y);
-	A->x /= length;
-	A->y /= length;
+	if(length)
+	{
+		A->x /= length;
+		A->y /= length;
+	}
+	else *A = (vector){0,0};
 }
