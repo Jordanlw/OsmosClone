ed4972f04a388c810de52277a65df7dcee199b0a
Jordan W <jordanw2@gmail.com>
2012-01-21 03:33:40 +1030

    Added basic code to background.c, needs work.

--- background.c
+++ background.c
@@ -3,21 +3,23 @@
 #include "header/background.h"
 #include "header/sdlstore.h"
 #include "header/blitCircle.h"
+#include "header/random.h"
+
+#define BG_OFFSET (randomResult(BG_INIT_MAX_SPACING,BG_INIT_MIN_SPACING))
 
 void backgroundBlit(void)
 {
 	SDL_Rect *camera = sdlStore(NULL,GET_CAMERA);
 	SDL_Surface *screen = sdlStore(NULL,GET_SCREEN);
-	int maxCol = camera->w / BG_WIDTH_SPACING;
-	int maxRow = camera->h / BG_HEIGHT_SPACING;
-	printf("c: %d, r: %d\n",maxCol,maxRow);
+	int maxCol = camera->w / BG_INIT_MAX_SPACING;
+	int maxRow = camera->h / BG_INIT_MAX_SPACING;
 	int col;
 	for(col = 0;col < maxCol;col++)
 	{
 		int row;
 		for(row = 0;row < maxRow;row++)
 		{
-			SDL_Rect pos = {col * BG_WIDTH_SPACING,row * BG_HEIGHT_SPACING,0,0};
+			SDL_Rect pos = {(col * maxCol) + BG_OFFSET,(row * maxRow) + BG_OFFSET,0,0};
 			blitCircle(BG_CIRCLE_RADIUS,screen,pos,(SDL_Color){255,255,255});
 		}
 	}
--- header/background.h
+++ header/background.h
@@ -1,5 +1,5 @@
-#define BG_WIDTH_SPACING 15
-#define BG_HEIGHT_SPACING 15
 #define BG_CIRCLE_RADIUS 5
+#define BG_INIT_MAX_SPACING 15
+#define BG_INIT_MIN_SPACING 5
 
 void backgroundBlit(void);
