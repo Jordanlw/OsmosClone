ea3cf46f2c3b7e145c953eee7412c45664f39e4d
Jordan W <jordanw2@gmail.com>
2012-01-22 02:53:20 +1030

    blitCircle() now handles middle row of circle correctly.

--- blitCircle.c
+++ blitCircle.c
@@ -11,15 +11,22 @@ int blitCircle(unsigned int radius,SDL_Surface *surface,SDL_Rect offset,SDL_Colo
 		return 1;
 	}
 	Uint32 pixelColor = SDL_MapRGB(surface->format,color.r,color.g,color.b);
+	//Draw middle line
 	int i;
+	for(i = 0;i < radius;i++)
+	{
+		SETPIXEL32(surface,offset.x + i,offset.y,pixelColor);
+		SETPIXEL32(surface,offset.x - i,offset.y,pixelColor);
+	}
+	
 	for(i = 0;i < radius;i++)
 	{
 		unsigned int width = (unsigned int)sqrt(8 * radius * i - 4 * i * i);
 		int j;
 		for(j = 0;j < width;j++)
 		{
-			SETPIXEL32(surface,offset.x - (width / 2) + j,offset.y - (radius - i) + 1,pixelColor);
-			SETPIXEL32(surface,offset.x - (width / 2) + j,offset.y + (radius - i) - 1,pixelColor);
+			SETPIXEL32(surface,offset.x - (width / 2) + j,offset.y - (radius - i),pixelColor);
+			SETPIXEL32(surface,offset.x - (width / 2) + j,offset.y + (radius - i),pixelColor);
 		}
 	}
 	return 0;
