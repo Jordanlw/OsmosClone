78f5988438e32b961cbd97bc1e8ee0ba6b72cc0f
Jordan W <jordanw2@gmail.com>
2012-01-26 08:49:55 +1030

    Background now tiles, still needs to have parallax effect, also added debug statements to background.c

--- background.c
+++ background.c
@@ -13,28 +13,44 @@ void backgroundBlit(void)
 	SDL_Surface *screen = sdlStore(NULL,GET_SCREEN);
 	bgData *initData = sdlStore(NULL,GET_BACKGROUND);
 	int *bgSizes = (int *)sdlStore(NULL,GET_BG_SIZE);
+	//DEBUG
+	//printf("cw / initw %d\n",camera->w / BG_INIT_W_SIZE);
 	//printf("bg: %p\n",bgSizes);
 	//Iterate over background layers
+	
 	int i;
 	for(i = 1;i <= BG_AMOUNT;i++)
 	{
-		int j;
-		for(j = 0;j < (MIN_INT(BG_INIT_H_SIZE,camera->h) / BG_INIT_MAX_SPACING);j++)
+		//For tiling
+		int mw;
+		for(mw = 0;mw < camera->w;mw += BG_INIT_W_SIZE)
 		{
-			int k;
-			for(k = 0;k < (MIN_INT(BG_INIT_W_SIZE,camera->w) / BG_INIT_MAX_SPACING);k++)
+			int my;
+			for(my = 0;my < camera->h;my += BG_INIT_H_SIZE)
 			{
-				SDL_Rect tmpRect = (SDL_Rect){GET_INIT_DATA(k,j,i,initData).pos.x,GET_INIT_DATA(k,j,i,initData).pos.y};
-				//DEBUG
-				//printf("blit: x %d y %d i %d j %d k %d\n",tmpRect.x,tmpRect.y,i,j,k);
+				int j;
+				for(j = 0;j < MIN_INT(BG_INIT_H_SIZE,camera->h) / BG_INIT_MAX_SPACING;j++)
+				{
+					int k;
+					for(k = 0;k < MIN_INT(BG_INIT_W_SIZE,camera->w) / BG_INIT_MAX_SPACING;k++)
+					{
+						SDL_Rect tmpRect = (SDL_Rect){GET_INIT_DATA(k,j,i,initData).pos.x + mw,GET_INIT_DATA(k,j,i,initData).pos.y + my};
+						//DEBUG
+						//printf("blit: x %d y %d i %d j %d k %d mw %d\n",tmpRect.x,tmpRect.y,i,j,k,mw);
 				
-				blitCircle(bgSizes[i - 1],screen,tmpRect,(SDL_Color){200,100,0});
-				//DEBUG
-				//printf("r %d x %d y %d\n",bgSizes[i],tmpRect.x,tmpRect.y);
-				//printf("bg-amnt %d\n",i);
+						blitCircle(bgSizes[i - 1],screen,tmpRect,(SDL_Color){200,100,0});
+						//DEBUG
+						//printf("r %d x %d y %d\n",bgSizes[i],tmpRect.x,tmpRect.y);
+						//printf("bg-amnt %d\n",i);
+					}
+				}
 			}
 		}
 	}
+	//DEBUG
+	//puts("");
+	//puts("DEBUG: calling exit() in background.c");
+	//exit(0);
 }
 
 
