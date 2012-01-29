a4d1196da29e8cb676bda28a75dff8263c425271
Jordan W <jordanw2@gmail.com>
2012-01-30 06:44:28 +1030

    Background offset added to background.c along with other small changes.

--- background.c
+++ background.c
@@ -4,8 +4,11 @@
 #include "header/sdlstore.h"
 #include "header/blitCircle.h"
 #include "header/random.h"
+#include "header/object.h"
+#include "header/objectStore.h"
 
 #define GET_INIT_DATA(x,y,layer,array) (array[(((y) * ((BG_INIT_W_SIZE) / (BG_INIT_MAX_SPACING)) + (x)) * (layer))])
+#define INIT_PLACEHOLDER (GET_INIT_DATA(k,j,i,initData).pos)
 
 void backgroundBlit(void)
 {
@@ -16,17 +19,27 @@ void backgroundBlit(void)
 	//DEBUG
 	//printf("cw / initw %d\n",camera->w / BG_INIT_W_SIZE);
 	//printf("bg: %p\n",bgSizes);
-	//Iterate over background layers
 	
+	int bgOffset[BG_AMOUNT];
 	int i;
+	for(i = 0;i < BG_AMOUNT;i++)
+	{
+		int mult = (camera->x / 5) * bgSizes[i];
+		bgOffset[i] = mult % camera->w;
+		//DEBUG
+		//printf("cx %d s %d o-b %d m %d\n",camera->x,bgSizes[i],bgOffset[i],mult);
+		printf("bg %d\n",bgOffset[i]);
+	}
+	
+	//Iterate over background layers
 	for(i = 1;i <= BG_AMOUNT;i++)
 	{
 		//For tiling
 		int mw;
 		for(mw = 0;mw < camera->w;mw += BG_INIT_W_SIZE)
 		{
-			int my;
-			for(my = 0;my < camera->h;my += BG_INIT_H_SIZE)
+			int mh;
+			for(mh = 0;mh < camera->h;mh += BG_INIT_H_SIZE)
 			{
 				int j;
 				for(j = 0;j < MIN_INT(BG_INIT_H_SIZE,camera->h) / BG_INIT_MAX_SPACING;j++)
@@ -34,7 +47,9 @@ void backgroundBlit(void)
 					int k;
 					for(k = 0;k < MIN_INT(BG_INIT_W_SIZE,camera->w) / BG_INIT_MAX_SPACING;k++)
 					{
-						SDL_Rect tmpRect = (SDL_Rect){GET_INIT_DATA(k,j,i,initData).pos.x + mw,GET_INIT_DATA(k,j,i,initData).pos.y + my};
+						int ix = INIT_PLACEHOLDER.x + mw - bgOffset[i - 1];
+						int iy = INIT_PLACEHOLDER.y + mh;
+						SDL_Rect tmpRect = (SDL_Rect){ix,iy};
 						//DEBUG
 						//printf("blit: x %d y %d i %d j %d k %d mw %d\n",tmpRect.x,tmpRect.y,i,j,k,mw);
 				
