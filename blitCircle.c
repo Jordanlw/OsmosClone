60a3c4510efb4cc87f01a76fe4bb50456c7355c8
Jordan W <jordanw2@gmail.com>
2012-05-08 03:17:18 +0930

    Changed SETPIXEL32 parameters in blitCircle.c

--- blitCircle.c
+++ blitCircle.c
@@ -17,8 +17,8 @@ int blitCircle(unsigned int radius,SDL_Surface *surface,SDL_Rect offset,SDL_Colo
 	int i;
 	for(i = 0;i < radius;i++)
 	{
-		SETPIXEL32(surface,offset.x + i,offset.y,pixelColor);
-		SETPIXEL32(surface,offset.x - i,offset.y,pixelColor);
+		SETPIXEL32(pixels,desc->size,offset.x + i,offset.y,pixelColor);
+		SETPIXEL32(pixels,desc->size,offset.x - i,offset.y,pixelColor);
 	}
 	
 	for(i = 0;i < radius;i++)
@@ -28,13 +28,13 @@ int blitCircle(unsigned int radius,SDL_Surface *surface,SDL_Rect offset,SDL_Colo
 		for(j = 0;j < width;j++)
 		{
 			//Top Right Quarter
-			SETPIXEL32(surface,offset.x + (j / 2),offset.y - radius + i,pixelColor);
+			SETPIXEL32(pixels,desc->size,offset.x + (j / 2),offset.y - radius + i,pixelColor);
 			//Top Left Quarter
-			SETPIXEL32(surface,offset.x - (j / 2),offset.y - radius + i,pixelColor);
+			SETPIXEL32(pixels,desc->size,offset.x - (j / 2),offset.y - radius + i,pixelColor);
 			//Bottom Right Quarter
-			SETPIXEL32(surface,offset.x + (j / 2),offset.y + radius - i,pixelColor);
+			SETPIXEL32(pixels,desc->size,offset.x + (j / 2),offset.y + radius - i,pixelColor);
 			//Bottom Left Quarter
-			SETPIXEL32(surface,offset.x - (j / 2),offset.y + radius - i,pixelColor);
+			SETPIXEL32(pixels,desc->size,offset.x - (j / 2),offset.y + radius - i,pixelColor);
 		}
 	}
 	return 0;
