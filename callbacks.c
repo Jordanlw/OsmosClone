6b06bfaee4b9b4fb2f348e495e32406f402bfb8c
Jordan W <jordanw2@gmail.com>
2012-05-18 07:58:00 +0930

    Added new functions to callbacks.c, also added semaphores to them and others.

--- callbacks.c
+++ callbacks.c
@@ -1,8 +1,11 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <ppapi/c/ppb_input_event.h>
 #include <ppapi/c/pp_errors.h>
 #include <ppapi/c/ppb_graphics_2d.h>
 #include <ppapi/c/pp_resource.h>
+#include <ppapi/c/ppb_instance.h>
+#include <ppapi/c/ppb_core.h>
 
 #include "header/sdlstore.h"
 #include "header/main.h"
@@ -16,19 +19,45 @@ void inputCallback(void *data,int32_t result)
 	if(returnValue == PP_ERROR_BADARGUMENT)
 	{
 		puts("DEBUG: inputCallback() 1");
+		exit(1);
 	}
 	else if(returnValue == PP_ERROR_NOTSUPPORTED)
 	{
 		puts("DEBUG: inputCallback() 2");
+		exit(1);
 	}
+	sem_post(readyData->sem);
 	return;
 }
 
 void g2DCallback(void *data,int32_t result)
 {
-	struct PPB_Graphics2D *g2DInterface = (struct PPB_Graphics2D *)sdlStore(NULL,GET_2D_INTERFACE);
+	PPB_Graphics2D *g2DInterface = (PPB_Graphics2D *)sdlStore(NULL,GET_2D_INTERFACE);
 	struct g2DCallbackData *readyData = (struct g2DCallbackData *)data;
 	PP_Resource screen = g2DInterface->Create(readyData->instance,readyData->size,readyData->flag);
 	sdlStore((void *)&screen,SET_SCREEN);
+	sem_post(readyData->sem);
+	return;
+}
+
+void bindCallback(void *data,int32_t result)
+{
+	PPB_Instance *instanceInterface = (PPB_Instance *)sdlStore(NULL,GET_INSTANCE_INTERFACE);
+	struct bindCallbackData *readyData = (struct bindCallbackData *)data;
+	if(instanceInterface->BindGraphics(readyData->instance,readyData->screen) == PP_FALSE)
+	{
+		puts("DEBUG: bindCallback() 1");
+		exit(1);
+	}
+	sem_post(readyData->sem);
+	return;
+}
+
+void timeCallback(void *data,int32_t result)
+{
+	PPB_Core *coreInterface = sdlStore(NULL,GET_CORE_INTERFACE);
+	struct timeCallbackData *readyData = (struct timeCallbackData *)data;
+	readyData->ticks = coreInterface->GetTimeTicks() * 1000;
+	sem_post(readyData->sem);
 	return;
 }
--- header/callbacks.h
+++ header/callbacks.h
@@ -1,3 +1,4 @@
+#include <semaphore.h>
 #include <ppapi/c/pp_instance.h>
 #include <ppapi/c/pp_bool.h>
 
@@ -5,6 +6,7 @@ struct inputCallbackData
 {
 	PP_Instance instance;
 	uint32_t flags;
+	sem_t *sem;
 };
 
 struct g2DCallbackData
@@ -12,8 +14,23 @@ struct g2DCallbackData
 	PP_Instance instance;
 	struct PP_Size *size;
 	PP_Bool flag;
-	
+	sem_t *sem;
+};
+
+struct bindCallbackData
+{
+	PP_Instance instance;
+	PP_Resource screen;
+	sem_t *sem;
+};
+
+struct timeCallbackData
+{
+	PP_TimeTicks ticks;
+	sem_t *sem;
 };
 
 void inputCallback(void *,int32_t);
 void g2DCallback(void *,int32_t);
+void bindCallback(void *,int32_t);
+void timeCallback(void *,int32_t);
